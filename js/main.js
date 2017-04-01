e4a5930785e645387ecf135b4702db62c82fe083
Kyle Maxey <ksmaxey@gmail.com>
2017-04-01 13:21:11 -0400

    fix linter errors and add jquery rollover function
    Fixed linter errors since the errors weren't showing up until installing
    an update the the linter package.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$('.skillbar').skillBars({
-		from: 0,
-		speed: 4000,
-		interval: 100,
-		decimals: 0,
-	});
-
+    $('.skillbar').skillBars({
+        from: 0,
+        speed: 4000,
+        interval: 100,
+        decimals: 0,
+    });
 });
 
 $('.type-it').typeIt({
     strings: ['Please hire me!']
-  });
+});
+
+//$(document).ready(function () {
+    //$('#me').mouseover(function () {
+        //$(this).replaceWith($('<img src="img/bw.jpg">'));
+    //});
+//});
+
+$('#me').mouseover(function () {
+    this.src = 'img/bw.jpg';
+}).mouseout(function () {
+    this.src = 'img/me.jpg';
+});
