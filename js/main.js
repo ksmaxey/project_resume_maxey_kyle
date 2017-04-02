085ce8e7a4daa258472863fc18bddc6cfd0f84f6
Kyle Maxey <ksmaxey@gmail.com>
2017-04-02 13:49:11 -0400

    clean up javascript file

--- js/main.js
+++ js/main.js
@@ -12,12 +12,6 @@ $('.type-it').typeIt({
     strings: ['Please hire me!']
 });
 
-//$(document).ready(function () {
-    //$('#me').mouseover(function () {
-        //$(this).replaceWith($('<img src="img/bw.jpg">'));
-    //});
-//});
-
 $('#me').mouseover(function () {
     this.src = 'img/bw.jpg';
 }).mouseout(function () {
